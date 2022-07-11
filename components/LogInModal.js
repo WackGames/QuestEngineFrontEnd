715b8e47365eee2d4731f51842ca5354c595ba55
AshJungers <97846504+AshJungers@users.noreply.github.com>
2022-07-11 17:08:34 -0600

    Added login/password container. Created index.html file
    index.html - I figured I needed an index page to include boostrap as font awesome icons links in head.

--- components/LogInModal.js
+++ components/LogInModal.js
@@ -1,8 +1,28 @@
 function LogInModal() {
-    return (
-      <div className="LogInModal">
-          <h1>Login Modal</h1>
-      </div>
+    return (         
+      <div class="login-container">
+        <h1>Login to Your Account</h1>
+
+    <div class="username-container">
+
+      <form id="login" method="get">
+        <i class="fa-solid fa-user"></i>
+        <input type="text" 
+        placeholder="Username" />
+        <br />
+    </div>
+
+    <div class="password-container">
+        <i class="fa-solid fa-lock"></i>
+        <input type="password" 
+        placeholder="Password" />
+      </form>
+
+      <br>
+
+      <button type="button" class="btn btn-outline-primary">LOGIN</button>
+    </div>
+  </div>
     );
   }
   
