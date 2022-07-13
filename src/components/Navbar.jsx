424019855bbe96755e1a3f28192a46ed12293380
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-07-12 19:28:15 -0600

    added password reset route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,13 @@ export default class Navbar extends React.Component {
   render () {
     return (
      
-<div class="navbar">
-  <div class="logo">
+<div className="navbar">
+  <div className="logo">
     <img src="https://firebasestorage.googleapis.com/v0/b/questengine-da9e6.appspot.com/o/QuestEngineLogo.png?alt=media&token=c2e1095d-b7f5-4cd9-a5a8-ec359a724c1cc" alt="logo" />
   </div>
-  <div class="buttons">
-    <button class="login">Login</button>
-    <button class="signup">Sign Up</button>
+  <div className="buttons">
+    <button className="login">Login</button>
+    <button className="signup">Sign Up</button>
   </div>
 </div>
     );
