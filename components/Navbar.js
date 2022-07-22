8471134e7e45ec2e31ac296ea1773d4278645f77
AshJungers <97846504+AshJungers@users.noreply.github.com>
2022-07-22 14:05:16 -0600

    Updating Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,26 @@
-import React from 'react';
-
-
+import React from "react";
 
 export default class Navbar extends React.Component {
-  render () {
+  render() {
     return (
-     
-<div className="navbar">
-  <div className="logo">
-    <img src="https://firebasestorage.googleapis.com/v0/b/questengine-da9e6.appspot.com/o/QuestEngineLogo.png?alt=media&token=c2e1095d-b7f5-4cd9-a5a8-ec359a724c1cc" alt="logo" />
-  </div>
-  <div className="buttons">
-    <button className="login">Login</button>
-    <button className="signup">Sign Up</button>
-  </div>
-</div>
+      <nav class="navbar-expand-md d-flex justify-content-between">
+        <a href="#">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/questengine-da9e6.appspot.com/o/QuestEngineLogo.png?alt=media&token=c2e1095d-b7f5-4cd9-a5a8-ec359a724c1cc"
+            alt="QuestEngine logo"
+          />
+        </a>
+
+        <li class="active">
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Login</a>
+        </li>
+      </nav>
     );
   }
 }
