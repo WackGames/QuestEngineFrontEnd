34289c17496431d3e9613dce945ad2a84d12eb6f
AshJungers <97846504+AshJungers@users.noreply.github.com>
2022-08-12 09:07:28 -0600

    Adding styling to login form

--- components/LogInModal.js
+++ components/LogInModal.js
@@ -2,26 +2,28 @@ import Link from "next/link";
 
 function LogInModal() {
   return (
-    <div>
+    <div class="main-container">
       <div class="login-page">
-        <h3>Login</h3>
+        <h3 class="mt-4 mb-4">Log In to Your Quest Engine Account!</h3>
         <div class="container">
           <form id="login" method="get">
-            <div>
+            <div class="username-login">
               <i class="fa-solid fa-user"></i>
               <input type="text" placeholder="Username" />
-              <br />
             </div>
-            <div class="password-container">
+            <div class="password-login">
               <i class="fa-solid fa-lock"></i>
               <input type="password" placeholder="Password" />
             </div>
           </form>
+
           <button type="button" class="btn btn-outline-primary">
             LOGIN
           </button>
           <div class="signup-link">
-            Not a member? <a href="">Sign up now</a>
+            <p>
+              Not a member? <a href="">Sign up now</a>
+            </p>
           </div>
         </div>
       </div>
