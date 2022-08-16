6985ee5c30c7fdf92e341028ed6940c1a4c181d8
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-08-15 23:03:17 -0600

    added basic styling for quizzes

--- pages/profile.js
+++ pages/profile.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import Quiz from '../components/Quiz'
+import Quizzes from '../components/Quizzes'
+
 export const getStaticProps = async () => {
   const res = await fetch("https://questengine.herokuapp.com/demo-quiz/");
   const data = await res.json();
@@ -16,9 +18,7 @@ export const getStaticProps = async () => {
 export default function Profile(props) {
   return (
     <div className={styles.container}>
-      <Quiz />
-      {props.quizzes.results.map(person => console.log(person.name))}
-
+      <Quizzes quizzes={props.quizzes.results} />
     </div>
   )
 }
