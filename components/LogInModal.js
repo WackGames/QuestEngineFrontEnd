94ce7281ca779b8561365b5eedaf8553733719a6
AshJungers <97846504+AshJungers@users.noreply.github.com>
2022-08-19 13:02:53 -0600

    Updated form

--- components/LogInModal.js
+++ components/LogInModal.js
@@ -1,27 +1,38 @@
 import Link from "next/link";
-import EmailData from "pages/EmailData";
+import { useState } from "react";
 
 function LogInModal() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const submitLogin = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
   return (
     <div className="main-container">
       <div className="login-page">
         <h4 className="mt-4 mb-4">Log In to Your Quest Engine Account!</h4>
         <div className="container">
-          <form id="login" method="get">
+          <form id="login" method="post">
             <div className="username-login">
               <i className="fa-solid fa-user"></i>
-              <input type="text" placeholder="Username" />
-              <EmailData />
+              <input type="text" placeholder="Username" value={username} />
             </div>
             <div className="password-login">
               <i className="fa-solid fa-lock"></i>
-              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="Password" value={password} />
             </div>
           </form>
 
-          <button type="submit" className="btn btn-outline-primary">
+          <button
+            type="submit"
+            className="btn btn-outline-primary"
+            onClick={(e) => submitLogin(e)}
+          >
             LOGIN
           </button>
+
           <div className="signup-link">
             <p>
               Not a member? <a href="">Sign up now</a>
