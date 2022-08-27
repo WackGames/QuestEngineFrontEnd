6cb74e53a18db8c3d3f8f87704d89db6df69569a
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-08-27 16:41:02 -0600

    added functionality for password reset confirm

--- pages/passreset/[uid]/[token].js
+++ pages/passreset/[uid]/[token].js
@@ -1,13 +1,16 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
+import styles from '../../../styles/ResetPasswordConfirm.module.scss'
 
 import { useState, useEffect } from "react"
 import { useRouter } from 'next/router'
 
-// import LogInModal from '../components/LogInModal'
+import ResetPasswordConfirmModal from '../../../components/ResetPasswordConfirmModal'
+
 export default function PassReset() {
-  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [retypePassword, setRetypePassword] = useState("")
+
   const [data, setData] = useState()
   const [loading, setLoading] = useState(true)
 
@@ -15,18 +18,14 @@ export default function PassReset() {
   
   function emailSubmit(e) {
       e.preventDefault();
-      console.log("HELLO THERE!")
+      console.log(router['query'].uid, router['query'].token)
   }
   useEffect(() => {
 }, [])
 
   return (
-    <div>
-      <h1>Password Reset</h1>
-      <form>
-      <input onChange={(e) => setEmail(e.target.value)} type="email" />
-      <button onClick={(e) => emailSubmit(e)}> Submit</button>
-      </form>
+    <div className={styles.container}>
+      <ResetPasswordConfirmModal />
     </div>
   )
 }
